06f6a9263e384e4fbd8bf06b8e6ed02fd9e44855
alexander <alexander.thequeen@gmail.com>
2025-09-25 18:57:14 +0300

    saving

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,14 @@
 function love.load()
 	print('DBG:', 'load')
-
 end
+
+function love.gamepadpressed(i, key)
+	print(">>", i, key)
+
+	if key == 'start' then
+		love.quit()
+	end
+end
+function love.gamepadreleased()
+end
+
